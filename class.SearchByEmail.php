ccb231b0568304065af77a1a5aa8f7ede229de00
Prabhanu <prabhanu96@gmail.com>
2019-05-28 11:38:18 +0530

    add strategy design pattern

---
+++ class.SearchByEmail.php
@@ -0,0 +1,9 @@
+<?php
+require_once('interface.Search.php');
+
+class SearchByEmail implements Search{
+    public function search($email){
+
+    }
+}
+?>
---
+++ class.SearchByName.php
@@ -0,0 +1,9 @@
+<?php
+require_once('interface.Search.php');
+
+class SearchByName implements Search{
+    public function search($name){
+
+    }
+}
+?>
